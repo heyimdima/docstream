5497a4a487c27c7349fddc07b6a351a39d55595a
heyimdima <bondardima@icloud.com>
2025-03-16 07:09:55 -0700

    sign in/sign up forms somewhat fixed

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -7,8 +7,8 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex items-center justify-center p-4 bg-neutral-950 relative">
-      <div className="w-full max-w-md relative z-10">{children}</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-md">{children}</div>
     </div>
   );
 }
