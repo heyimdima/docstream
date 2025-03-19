0d59f872ac36729a78211ceaacd2dfa9ce9198fb
heyimdima <bondardima@icloud.com>
2025-03-19 08:46:13 -0700

    123

--- frontend/src/types/chat.ts
+++ frontend/src/types/chat.ts
@@ -1,6 +1,15 @@
+import { Message } from "@/types/message";
+import { ChatDocumentation } from "@/types/chat-documentation";
+
 export interface Chat {
   id: string;
   user_id: string;
   title: string;
   created_at: Date;
+}
+
+export interface ExistingChat {
+  id: string;
+  messages: Message[];
+  documentations: ChatDocumentation[];
 }
---
+++ frontend/src/types/documentation.ts
@@ -0,0 +1,8 @@
+export interface Documentation {
+  id: string;
+  source_url: string;
+  name: string;
+  embedding_id: string;
+  updated_at: Date;
+  created_at: Date;
+}
